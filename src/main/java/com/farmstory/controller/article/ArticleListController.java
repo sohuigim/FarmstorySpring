b9004418a27660caa27104d5128858e6d596d849
subin231 <subinasa231@gmail.com>
2024-09-12 16:28:19 +0900

    CSB_2024-09-12_Event update and Calendar merge

--- src/main/java/com/farmstory/controller/article/ArticleListController.java
+++ src/main/java/com/farmstory/controller/article/ArticleListController.java
@@ -1,8 +1,14 @@
 package com.farmstory.controller.article;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ArticleListController {
+
+    @GetMapping("/event/EventList")
+    public String EventList(){
+        return "/event/EventList";
+    }
 
 }
