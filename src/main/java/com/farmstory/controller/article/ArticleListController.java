885fd14752f7a089061109a446201fa2e4fb1c94
sohuigim <aisurudake@gmail.com>
2024-09-13 12:08:50 +0900

    GSH_2024_09_13_CROP_UPDATE

--- src/main/java/com/farmstory/controller/article/ArticleListController.java
+++ src/main/java/com/farmstory/controller/article/ArticleListController.java
@@ -6,9 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ArticleListController {
 
+    //이벤트
     @GetMapping("/event/EventList")
     public String EventList(){
         return "/event/EventList";
     }
+
+    //커뮤니티
+    @GetMapping("/community/CommunityNotice")
+    public String CommunityNotice(){
+        return "/community/CommunityNotice";
+    }
+
+
+
+
 
 }
