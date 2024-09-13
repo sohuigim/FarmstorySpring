812d87d7498b89946349f04264956d51e51ad0fc
sohuigim <aisurudake@gmail.com>
2024-09-13 17:43:13 +0900

    GSH_2024_09_13_CROP_UPDATE2

--- src/main/java/com/farmstory/controller/article/crop/CropStroyController.java
+++ src/main/java/com/farmstory/controller/article/crop/CropStroyController.java
@@ -1,32 +1,42 @@
 package com.farmstory.controller.article.crop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CropStroyController {
 
-    //농작물이야기
+    //농작물이야기 // 메인
     @GetMapping("/crop/CropStory")
-    public String cropStory() {
+    public String cropStory(Model model) {
+        String str1 = "b201";
+        model.addAttribute("str1", str1);
         return "/crop/CropStory";
     }
 
     //글쓰기
     @GetMapping("/crop/talk/CropWrite")
-    public String CropStoryWriter() {
+    public String CropStoryWriter(Model model) {
+        String str1 = "b201";
+        model.addAttribute("str1", str1);
+
         return "/crop/talk/CropWrite";
     }
 
     //글보기
     @GetMapping("/crop/talk/CropView")
-    public String CropStoryView() {
+    public String CropStoryView(Model model) {
+        String str1 = "b201";
+        model.addAttribute("str1", str1);
         return "/crop/talk/CropView";
     }
 
     //글수정
     @GetMapping("/crop/talk/CropModify")
-    public String CropStoryModify() {
+    public String CropStoryModify(Model model) {
+        String str1 = "b201";
+        model.addAttribute("str1", str1);
         return "/crop/talk/CropModify";
     }
 
