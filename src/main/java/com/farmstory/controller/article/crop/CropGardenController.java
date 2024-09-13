2b9b0bf4cd72d2627a2e74341a9cc7f29ea154a4
sohuigim <aisurudake@gmail.com>
2024-09-13 17:43:13 +0900

    GSH_2024_09_13_CROP_UPDATE2

--- src/main/java/com/farmstory/controller/article/crop/CropGardenController.java
+++ src/main/java/com/farmstory/controller/article/crop/CropGardenController.java
@@ -1,6 +1,7 @@
 package com.farmstory.controller.article.crop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +9,9 @@ public class CropGardenController {
 
     //텃밭가꾸기
     @GetMapping("/crop/CropGarden")
-    public String CropGarden() {
+    public String CropGarden(Model model) {
+        String str1 = "b202";
+        model.addAttribute("str1", str1);
         return "/crop/CropGarden";
     }
 
--- src/main/java/com/farmstory/controller/article/crop/CropReturnfarmController.java
+++ src/main/java/com/farmstory/controller/article/crop/CropReturnfarmController.java
@@ -1,6 +1,7 @@
 package com.farmstory.controller.article.crop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +9,9 @@ public class CropReturnfarmController {
 
     //귀농학교
     @GetMapping("/crop/CropReturnfarm")
-    public String CropReturnfarm() {
+    public String CropReturnfarm(Model model) {
+        String str1 = "b203";
+        model.addAttribute("str1", str1);
         return "/crop/CropReturnfarm";
     }
 
