7dbc7464e809055c53bf8f488e05926272f2386f
gkdntn <gkdntn@gmail.com>
2024-09-13 14:21:37 +0900

    HJH_2024_09_13_market

--- src/main/java/com/farmstory/controller/article/community/CommunityFaqController.java
+++ src/main/java/com/farmstory/controller/article/community/CommunityFaqController.java
@@ -12,21 +12,5 @@ public class CommunityFaqController {
         return "/community/CommunityFaq";
     }
 
-    // 글쓰기
-    @GetMapping("/community/CommunityWrite")
-    public String CommunityWrite(){
-        return "/community/CommunityWrite";
-    }
-
-    // 글수정
-    @GetMapping("/community/CommunityModify")
-    public String CommunityModify(){
-        return "/community/CommunityModify";
-    }
-
-    // 글보기
-    @GetMapping("/community/CommunityView")
-    public String CommunityView(){
-        return "/community/CommunityView";
-    }
+ 
 }
