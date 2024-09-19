f31de6a07a8b6f83827038cecb5cee4a162e6b46
sohuigim <aisurudake@gmail.com>
2024-09-19 10:58:21 +0900

    GSH_2024_09_19_Article_update

--- src/main/java/com/farmstory/service/ArticleService.java
+++ src/main/java/com/farmstory/service/ArticleService.java
@@ -1,4 +1,11 @@
 package com.farmstory.service;
 
+import com.farmstory.repository.article.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class ArticleService {
+
 }
