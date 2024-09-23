6d0d490afb56db293324845e2d43b5819579ed29
DoyoungLee9720 <dlehdud224@gmail.com>
2024-09-23 17:07:40 +0900

    adminuserlist and adminuserlisthtml

--- src/main/java/com/farmstory/controller/admin/AdminUserListController.java
+++ src/main/java/com/farmstory/controller/admin/AdminUserListController.java
@@ -1,13 +1,25 @@
 package com.farmstory.controller.admin;
 
+import com.farmstory.dto.UserDTO;
+import com.farmstory.entity.User;
+import com.farmstory.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class AdminUserListController {
 
+    private final UserService userService;
+
     @GetMapping("/admin/UserList")
-    public String AdminUser() {
+    public String AdminUser(Model model) {
+        List<UserDTO> userDto = userService.selectUsers();
+        model.addAttribute("userDto", userDto);
         return "/admin/user/UserList";
     }
 
