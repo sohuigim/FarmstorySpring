db4fd59b87efdfb56cad824f4dc69cb4728480d1
gkdntn <gkdntn@gmail.com>
2024-09-25 11:40:00 +0900

    marketList_marketView

--- src/main/java/com/farmstory/repository/product/ProductRepositoryCustom.java
+++ src/main/java/com/farmstory/repository/product/ProductRepositoryCustom.java
@@ -1,6 +1,10 @@
 package com.farmstory.repository.product;
 
+import com.farmstory.dto.MarketPageRequestDTO;
 import com.farmstory.entity.Product;
+import com.querydsl.core.Tuple;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,4 +12,6 @@ public interface ProductRepositoryCustom {
 
     public List<Product> selectProducts();
     public Product selectProduct(int prodNo);
+
+    public Page<Tuple> selectProductAllForList(MarketPageRequestDTO marketPageRequestDTO , Pageable pageable, int catetype);
 }
