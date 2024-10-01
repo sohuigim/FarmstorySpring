36ba02567851ecda9aa40221b5b12d531cccc0f0
subin231 <subinasa231@gmail.com>
2024-10-01 17:33:07 +0900

    update article

--- src/main/java/com/farmstory/service/OrderService.java
+++ src/main/java/com/farmstory/service/OrderService.java
@@ -7,6 +7,7 @@ import com.farmstory.repository.OrderRepository;
 import com.farmstory.repository.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,28 +21,29 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final ProductRepository productRepository;
     public void insertOrder(OrderDTO orderDTO) {
-        Product product = productRepository.findById(orderDTO.getProdNo())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid product ID"));
-
-        // OrderDTO -> Order 엔티티로 변환할 때 Product 객체를 전달
-        Order entity = orderDTO.toEntity(product);
-        orderRepository.save(entity);
+//        Product product = productRepository.findById(orderDTO.getProdNo())
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid product ID"));
+//
+//        // OrderDTO -> Order 엔티티로 변환할 때 Product 객체를 전달
+//        Order entity = orderDTO.toEntity(product);
+        orderRepository.save(orderDTO.toEntity());
     }
 
     public List<OrderDTO> selectOrders() {
-        List<Order> orders = orderRepository.findAll();
-
-        List<OrderDTO> orderDTOs = orders.stream()
-                .map(entity -> {
-                    OrderDTO dto = entity.toDTO();
-                    if (entity.getProduct() != null) {
-                        dto.setProdName(entity.getProduct().getProdName()); // Product 이름 설정
-                    }
-                    return dto;
-                })
-                .collect(Collectors.toList());
-
-        return orderDTOs;
+//        List<Order> orders = orderRepository.findAll();
+//
+//        List<OrderDTO> orderDTOs = orders.stream()
+//                .map(entity -> {
+//                    OrderDTO dto = entity.toDTO();
+//                    if (entity.getProduct() != null) {
+//                        dto.setProdName(entity.getProduct().getProdName()); // Product 이름 설정
+//                    }
+//                    return dto;
+//                })
+//                .collect(Collectors.toList());
+//
+//        return orderDTOs;
+        return null;
     }
     public void DeleteOrders(List<String> orderIds) {
         for(String id : orderIds) {
